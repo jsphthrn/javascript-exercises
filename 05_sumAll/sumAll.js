efd1c023d2aee3bbb73ea000d726716581548ba1
José Yáñez <jlyp_sn@protonmail.com>
2025-02-23 13:49:33 -0600

    Finish exercise 7.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || num1 < 0 || num2 < 0 || num1 !== Number(num1.toFixed(0)) || num2 !== Number(num2.toFixed(0))) {
+        return "ERROR";
+    } else {
+        let finalSum = 0;
+        if (num1 > num2) {
+            for (let i = num2; i <= num1; i++) {
+                finalSum += i;
+            }
+        } else { 
+            for (let i = num1; i <= num2; i++) {
+                finalSum += i;
+            }
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+    let tempC = (tempF - 32) / 1.8;
+    if (tempC === Number(tempC.toFixed(0))) {
+      return tempC; 
+    } else { 
+      return Number(tempC.toFixed(1));
+    }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+    let tempF = tempC * 1.8 + 32;
+    if (tempF === Number(tempF.toFixed(0))) {
+      return tempF;
+    } else { 
+      return Number(tempF.toFixed(1));
+    }
 };
 
 // Do not edit below this line
