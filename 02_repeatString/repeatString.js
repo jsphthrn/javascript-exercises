96a85d2c24dcc81fbc75857cb30626f5e349952e
José Yáñez <jlyp_sn@protonmail.com>
2025-02-23 12:14:42 -0600

    Finish first 4 exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberRepeats) {
+    if (numberRepeats < 0) {
+        return ("ERROR");
+    } else {
+        let stringRepeat = "";
+        for (let i = 1; i <= numberRepeats; i++) {
+            stringRepeat += string;
+        }
+        return stringRepeat;
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (const value of args) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === value && typeof array[i] === typeof value) {
+                array.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
