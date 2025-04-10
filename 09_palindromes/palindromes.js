7f69ced49728f075d00f3effa7858329d469d19e
Jose Yanez <jlyp_sn@protonmail.com>
2025-04-10 00:01:10 -0600

    Finish palindromes exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    REGULAR_EXPRESSION = /\w+/g;
+    str = str.toLowerCase();
+    str = str.match(REGULAR_EXPRESSION);
+    str = str.join('');
+    let a = str.split('');
+    a.reverse();
+    a = a.join('');
+    a.toString();
+    return (str === a) ? true : false;
+    };
 
 // Do not edit below this line
 module.exports = palindromes;
