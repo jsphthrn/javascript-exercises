a1daa5558d103cc35e2dbdf7b3f71ac03f3e683d
Jose Yanez <jlyp_sn@protonmail.com>
2025-04-10 00:34:30 -0600

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = 1 * num;
+    if (num < 0) {
+        return "OOPS";
+    } else if (num == 0) {
+        return 0;
+    } else {
+        let series = [1,1];
+        for (let i = 2; i < num; i++){
+            series[i] = series[i-1] + series[i-2];
+        }
+        return series[num-1];
+    }
 };
 
 // Do not edit below this line
