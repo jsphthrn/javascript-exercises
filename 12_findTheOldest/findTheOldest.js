8ff5c4757cd897557492b039bcd57fb5d80a936a
Jose Yanez <jlyp_sn@protonmail.com>
2025-04-10 01:24:01 -0600

    Finish findTheOldest exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const current = new Date();
+    arr.sort((personA, personB) => {
+        let deathA;
+        let deathB;
+        (!personA.yearOfDeath) ? deathA = current.getFullYear() : deathA = personA.yearOfDeath;
+        (!personB.yearOfDeath) ? deathB = current.getFullYear() : deathB = personB.yearOfDeath;
+        return ((deathA - personA.yearOfBirth) > (deathB - personB.yearOfBirth)) ? -1 : 1;
+    });
 
+    return arr[0];
 };
 
 // Do not edit below this line
