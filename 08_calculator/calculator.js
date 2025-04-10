e4dd7d6dbb8c9d4e2e9e0b2b079a815c5c097917
Jose Yanez <jlyp_sn@protonmail.com>
2025-04-09 22:59:18 -0600

    Finish calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(...operands) {
+    let operationResult = 0;
+    operands.forEach(element => {
+      operationResult += element;
+    });
+    return operationResult;
 };
 
-const subtract = function() {
-	
+const subtract = function(...operands) {
+	  let operationResult = 2 * operands[0];
+    operands.forEach(element => {
+      operationResult -= element;
+    });
+    return operationResult;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let operationResult = 0;
+    arr.forEach(element => {
+      operationResult += element;
+    });
+    return operationResult;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let operationResult = 1;
+    arr.forEach(element => {
+      operationResult *= element;
+    });
+    return operationResult;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <= a; i++){
+      result *= i;
+    }
+    return result;
+  }  
 };
 
 // Do not edit below this line
